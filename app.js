fba917bee60ba7b6aeefe8c7a955d065da0882db
unknown <yasmine0927@hotmail.fr>
2023-09-19 12:01:03 +0200

    init_back

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json');
+const swaggerAutogen = require('swagger-autogen');
+
+const app = express();
+const outputFile = './swagger_output.json'
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+swaggerAutogen(outputFile, []) // rajouter les routers dès qu'ils existes
+app.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(8000, () => {
+    console.log(`Le serveur est en écoute sur le port 8000`);
+});
